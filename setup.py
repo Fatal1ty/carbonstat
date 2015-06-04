472e9fad01f4032b21c664ee755c95a3063be7cf
Alexander Tikhonov <fatal1ty.rnd@gmail.com>
2015-06-04 18:04:54 +0300

    Add setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='carbonstat',
+      version='0.1',
+      description='Metric collection agent for Carbon',
+      author='Alexander Tikhonov',
+      author_email='random.gauss@gmail.com',
+      py_modules=['carbonstat'])
