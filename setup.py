054d798ba5c206e1ffd950acc6ceb244f0e37af1
Alexander Tikhonov <fatal1ty.rnd@gmail.com>
2015-06-05 20:00:23 +0300

    Version 0.2.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='carbonstat',
-      version='0.2',
+      version='0.2.1',
       description='Metric collection agent for Carbon',
       license='GPL',
       author='Alexander Tikhonov',
