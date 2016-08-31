060fae8f89611cb1dfe29b98b45e1efd2587ff4c
Alexander Tikhonov <fatal1ty.rnd@gmail.com>
2016-08-31 11:04:49 +0300

    Fix installing via pip

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='carbonstat',
-      version='0.4.5',
+      version='0.4.6',
       description='Metric collection agent for Carbon',
       long_description=open('README.rst').read(),
       platforms='all',
